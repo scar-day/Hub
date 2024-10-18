65a19dfdfb5061f67b60ba303b6549446bde599a
ScarDay <alexeyishutov@gmail.com>
2024-10-19 01:13:37 +0400

    usage minecrell for bungee

--- bungee/build.gradle.kts
+++ bungee/build.gradle.kts
@@ -1,7 +1,18 @@
+plugins {
+    id("net.minecrell.plugin-yml.bungee") version("0.6.0")
+}
+
 dependencies {
     implementation(project(":common"))
 
     implementation("eu.okaeri:okaeri-platform-bungee:0.4.39")
 
     compileOnly("io.github.waterfallmc:waterfall-api:1.20-R0.1-SNAPSHOT")
+}
+
+bungee {
+    main = "dev.scarday.Main"
+    name = "Hub"
+    version = "2.1-BETA"
+    author = "ScarDay"
 }
